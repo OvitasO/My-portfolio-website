dab970ff8d15343bc6786ba63520b118fa9c1407
Vitas <Depsterer@gmail.com>
2026-07-12 14:08:49 +0100

    Made windows draggable

--- js/windows.js
+++ js/windows.js
@@ -1,31 +1,64 @@
 const windowList = [
   {
     id: 'linksWindow',
-    open: false
+    decorationId: 'linksDecoration'
   },
 
   {
     id: 'projectsWindow',
-    open: false
+    decorationId: 'projectsDecoration'
   },
 
   {
     id: 'contactWindow',
-    open: false
+    decorationId: 'contactDecoration'
   }
 ]
 
 const contactBtn = document.getElementById('contactBtn');
-contactCloseBtn = document.getElementById('contactCloseBtn');
+const contactCloseBtn = document.getElementById('contactCloseBtn');
+const contactDecoration = document.getElementById('contactDecoration');
+
 
 function openWindow(i) {
-      document.getElementById(`${windowList[i].id}`)
-        .classList.add('openedWindow');
+  document.getElementById(`${windowList[i].id}`)
+    .classList.add('openedWindow');
     }
 
 function closeWindow(i) {
   document.getElementById(`${windowList[i].id}`)
     .classList.remove('openedWindow');
+}
+
+function moveWindow(event, i) {
+  const activeWindow = document.getElementById(`${windowList[i].id}`);
+  const pointerInitial = {'X': event.clientX, 'Y': event.clientY};
+  const windowInitial = activeWindow.getBoundingClientRect();
+  const windowInitialX = windowInitial.left;
+  const windowInitialY = windowInitial.top;
+
+  activeWindow.classList.add('dragging');
+
+  function pointerMove(event) {
+    const pointerMoveX = event.clientX - pointerInitial.X;
+    const pointerMoveY = event.clientY - pointerInitial.Y;
+
+    const windowNewX = windowInitialX + pointerMoveX; 
+    const windowNewY = windowInitialY + pointerMoveY;
+
+    activeWindow.style.left = `${windowNewX}px`;
+    activeWindow.style.top = `${windowNewY}px`;
+  }
+
+  function pointerUp() {
+    document.removeEventListener('pointermove', pointerMove);
+    document.removeEventListener('pointerup', pointerUp);
+    activeWindow.classList.remove('dragging');
+  }
+  
+  document.addEventListener('pointermove', pointerMove);
+  document.addEventListener('pointerup', pointerUp);
+  
 }
 
 contactBtn.addEventListener('click', () => {
@@ -34,4 +67,8 @@ contactBtn.addEventListener('click', () => {
 
 contactCloseBtn.addEventListener('click', () => {
   closeWindow(2);
+})
+
+contactDecoration.addEventListener('pointerdown', (event) => {
+  moveWindow(event, 2);
 })
