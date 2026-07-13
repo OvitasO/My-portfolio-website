0a9ef52835742ccade9cd90a692188008ec3f3a2
Vitas <Depsterer@gmail.com>
2026-07-13 19:40:33 +0100

    hotkeys demo now plays from the beggining every time

--- js/projects.js
+++ js/projects.js
@@ -3,6 +3,7 @@ const youtubeViewer = document.getElementById('youtubeViewer');
 
 const hotkeyView = document.getElementById('hotkeyView');
 const hotkeyViewer = document.getElementById('hotkeyViewer');
+const hotkeyDemo = document.getElementById('hotkeyDemo');
 
 
 youtubeView.addEventListener('click', () => {
@@ -15,6 +16,8 @@ youtubeViewer.addEventListener('click', () => {
 
 hotkeyView.addEventListener('click', () => {
   hotkeyViewer.classList.add('shown');
+  hotkeyDemo.currentTime = 0;
+  hotkeyDemo.play();
 });
 
 hotkeyViewer.addEventListener('click', () => {
