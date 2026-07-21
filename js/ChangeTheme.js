51920b14c58fcdcb92ccbf71b162c1dc6e29fab0
Vitas <Depsterer@gmail.com>
2026-07-21 12:41:39 +0100

    change theme logic done, mostly only css left

--- js/ChangeTheme.js
+++ js/ChangeTheme.js
@@ -1,8 +1,10 @@
 import { audioTypes, playAudio } from "./audio.js";
 
 const toggleThemeBtn = document.getElementById('toggleThemeBtn');
+const toggleThemeIcon = document.getElementById('toggleThemeIcon');
 
-let darkTheme = false;
+let darkTheme = JSON.parse(localStorage.getItem('theme')) ?? false;
+updateTheme();
 
 toggleThemeBtn.addEventListener('click', () => {
   darkTheme = !darkTheme
@@ -13,4 +15,17 @@ toggleThemeBtn.addEventListener('click', () => {
   else {
     playAudio(audioTypes.lightThemeAudio, 0.2, 1, 0);
   }
+  updateTheme();
+  localStorage.setItem('theme', `${darkTheme}`);
 })
+
+function updateTheme() {
+  if (darkTheme) {
+    document.body.classList.add('dark');
+    toggleThemeIcon.src = '../images/darkMode/moon.svg';
+  }
+  else {
+    document.body.classList.remove('dark');
+    toggleThemeIcon.src = '../images/lightMode/brightness.svg';
+  }
+}
